6b7458dcc29844e31894fa9757ec09e5207b818f
ABENK21 <50725114+NULLBYTE21@users.noreply.github.com>
2019-07-01 09:36:07 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+figlet -f slant "Wait" | lolcat
+      pkg install figlet
+figlet -f slant "Wait" | lolcat
+      pkg install toilet
+figlet -f slant "Wait" | lolcat
+      gem install lolcat
+      figlet -f slant "Wait" | lolcat
+      pkg install wireshark
+      figlet -f slant "Wait" | lolcat
+      pkg install tshark
+      figlet -f slant "Wait" | lolcat
+      pkg install aircrack-ng
+      figlet -f slant "Wait" | lolcat
+      pkg install bully
+      figlet -f slant "Wait" | lolcat
+      pkg install wireless-tools
+      figlet -f slant "Wait" | lolcat
+      pkg install net-tools
